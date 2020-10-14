c4fad00cbf9bceced32630db659047efcca561c0
James Little <james@destinationtoolbox.com>
2020-10-13 22:49:51 -0500

    Remove double json encoding on address formatting

--- TransmailClient.php
+++ TransmailClient.php
@@ -129,9 +129,6 @@ class TransmailClient{
 	//send actual message
 	public function send(){
 
-		echo json_encode($this->data);
-		echo "<br>key: ". $this->key;
-
 		// Prepare new cURL resource
 		$crl = curl_init('https://api.transmail.com/v1.1/email');
 		curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
@@ -239,7 +236,7 @@ class TransmailClient{
 			
 		}
 
-		return json_encode($finalarray);
+		return $finalarray;
 	} 
 
 }
